3ccab6d1660ac580eee3e6e294455b33a951d68a
ShokhrukhTurgunov <77617034+ShokhrukhTurgunov@users.noreply.github.com>
2021-08-20 10:26:29 +0500

    add setInterval files

---
+++ main.js
@@ -0,0 +1,31 @@
+const btn1 = document.querySelector('.right');
+const btn2 = document.querySelector('.left');
+console.log(btn2);
+
+
+function btnAnimation1 () {
+  const box = document.querySelector('.mini-box');
+  let pos = 0;
+  const id = setInterval(animFunc, 50)
+
+  function animFunc() {
+    if (pos === 200) {
+      clearInterval(id);
+    }else {
+      pos++;
+      box.style.left = pos + "px";
+      box.style.top = pos + "px";
+    }
+  }
+};
+
+
+function btnAnimation2 () {
+
+    const box = document.querySelector('.mini-box');
+    box.classList.toggle('class')
+
+};
+
+btn1.addEventListener("click", btnAnimation1);
+btn2.addEventListener("click", btnAnimation2);
